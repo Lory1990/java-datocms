39e42466db32734aaa96e731f2a744a2ea99d863
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2025-10-03 23:17:36 +0200

    fix: worklfow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("signing")
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 group = "io.github.lory1990"
@@ -29,60 +30,36 @@ tasks.test {
     useJUnitPlatform()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
+mavenPublishing {
+    coordinates("io.github.lory1990", "java-datocms", version.toString())
 
-            groupId = project.group.toString()
-            artifactId = "java-datocms"
-            version = project.version.toString()
+    pom {
+        name.set("DatoCMS Java Client")
+        description.set("A lightweight Java client library for interacting with the DatoCMS GraphQL API")
+        url.set("https://github.com/Lory1990/java-datocms")
 
-            pom {
-                name.set("DatoCMS Java Client")
-                description.set("A lightweight Java client library for interacting with the DatoCMS GraphQL API")
-                url.set("https://github.com/Lory1990/java-datocms")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("Lory1990")
-                        name.set("Lorenzo De Francesco")
-                        email.set("Lory1990@users.noreply.github.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/Lory1990/java-datocms.git")
-                    developerConnection.set("scm:git:ssh://github.com/Lory1990/java-datocms.git")
-                    url.set("https://github.com/Lory1990/java-datocms")
-                }
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
             }
+        }
+
+        developers {
+            developer {
+                id.set("Lory1990")
+                name.set("Lorenzo De Francesco")
+                email.set("Lory1990@users.noreply.github.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/Lory1990/java-datocms.git")
+            developerConnection.set("scm:git:ssh://github.com/Lory1990/java-datocms.git")
+            url.set("https://github.com/Lory1990/java-datocms")
         }
     }
 
-    repositories {
-        maven {
-            name = "OSSRH"
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-
-            credentials {
-                username = project.findProperty("sonatypeUsername")?.toString() ?: System.getenv("SONATYPE_USERNAME")
-                password = project.findProperty("sonatypePassword")?.toString() ?: System.getenv("SONATYPE_PASSWORD")
-            }
-        }
-    }
-}
-
-signing {
-    useGpgCmd()
-    sign(publishing.publications["mavenJava"])
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
